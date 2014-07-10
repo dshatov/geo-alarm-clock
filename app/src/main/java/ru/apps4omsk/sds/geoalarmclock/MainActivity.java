3c49a62cdf0b570f65b923a34b80bbe99a62156e
Ildar Karymov <ildarkarymoff@gmail.com>
2014-07-11 00:51:06 +0600

    Замутил определение местоположение по беспроводным сетям

--- app/src/main/java/ru/apps4omsk/sds/geoalarmclock/MainActivity.java
+++ app/src/main/java/ru/apps4omsk/sds/geoalarmclock/MainActivity.java
@@ -1,17 +1,106 @@
 package ru.apps4omsk.sds.geoalarmclock;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    LocationManager locationManager;
+    String locationProvider = LocationManager.NETWORK
+
+    _PROVIDER;
+    LocationListener locationListener;
+
+    TextView coordTextView;
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            return true;
+        }
+
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        if (isSignificantlyNewer) {
+            return true;
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        boolean isFromSameProvider = isSameProvider(location.getProvider(), currentBestLocation.getProvider());
+
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Entry point
+        coordTextView = (TextView) findViewById(R.id.CoordTextView);
+            try {
+                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.d("TEST_GEO_LOCATION", location.getLatitude() + " and " + location.getLongitude());
+                        coordTextView.setText(location.getLatitude() + " and " + location.getLongitude());
+                        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String s) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String s) {
+
+                    }
+                };
+            } catch (Exception e) {
+                Log.e("TEST_GEO_LOCATION", "Network unavailable!");
+            }
+
+        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
     }
 
 
